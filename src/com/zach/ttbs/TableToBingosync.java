d216efd31a57321d469982554ac920566af6f491
xMGZx <huntingmanzach@gmail.com>
2018-12-11 22:27:38 -0500

    Removed unused import

--- src/com/zach/ttbs/TableToBingosync.java
+++ src/com/zach/ttbs/TableToBingosync.java
@@ -13,7 +13,6 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
